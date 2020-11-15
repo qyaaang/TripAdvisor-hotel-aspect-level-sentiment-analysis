024672ecdc0d17b738d29d31c4e3fac7a42d981b
Qun Yang <yangqunstruct@gmail.com>
2020-11-16 01:11:29 +1300

    Update

--- train.sh
+++ train.sh
@@ -1,12 +1,13 @@
 dataset="TripAdvisor_hotel"
-model_name="ATAE_BiLSTM"
+model_names=("AT_GRU" "ATAE_GRU")
 num_sample=4000
 models=("1" "2" "3" "4")
-num_epoch=1000
+max_seq_len=80
+hidden_dim=30
+num_epoch=100
 optimizers=("SGD" "Adam" "AdaBelief")
-learning_rate=0.001
+learning_rates=(0.001)
 for model in "${models[@]}"; do
-  echo "$model"
   if [ "$model" == "1" ]; then
     fracs=(0.35 0.35 0.3)
   elif [ "$model" == "2" ]; then
@@ -16,10 +17,17 @@ for model in "${models[@]}"; do
   else
     fracs=(0.25 0.15 0.6)
   fi
-  for optimizer in "${optimizers[@]}"; do
-    echo "$optimizer"
-    python3 train.py --model_name $model_name --dataset $dataset --optimizer "$optimizer" --num_epoch $num_epoch \
-                     --num_sample $num_sample --frac_pos "${fracs[0]}" --frac_neu "${fracs[1]}" --frac_neg "${fracs[2]}" \
-                     --learning_rate $learning_rate --testset "$model"
+  for model_name in "${model_names[@]}"; do
+    for optimizer in "${optimizers[@]}"; do
+      for learning_rate in "${learning_rates[@]}"; do
+        printf "\033[1;32mModel:\t%s\nModel_name:\t%s\nOptimizer:\t%s\nLearning_rate:\t%s\n\033[0m" \
+               "$model" "$model_name" "$optimizer" "$learning_rate"
+        python3 train.py --model_name "$model_name" --dataset $dataset \
+                         --learning_rate "$learning_rate" \
+                         --num_sample $num_sample --frac_pos "${fracs[0]}" --frac_neu "${fracs[1]}" --frac_neg "${fracs[2]}" \
+                         --max_seq_len $max_seq_len --hidden_dim $hidden_dim --num_epoch $num_epoch \
+                         --optimizer "$optimizer" --testset "$model"
+      done
     done
+  done
 done
